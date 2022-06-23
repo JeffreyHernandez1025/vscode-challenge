e8c9db20cfc9463be3479c538decf5dd8a1c0fe2
TXT-66 <txt-45@TXT-66.lan>
2022-06-23 03:02:52 -0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+function me() {
+  document.getElementById('question').innerHTML = 'Who am I?'
+  document.getElementById('para').textContent =
+    'I am Jeffrey Hernandez, an incoming junior in Animo South Los Angeles Charter Highschool, who likes to spend time learning new things, like coding.'
+}
+function become() {
+  document.getElementById('question').innerHTML = 'What do I want to become?'
+  document.getElementById('para').innerHTML =
+    'I want to become someone who is able to live comfortably in a finincial sense. Coming from a family without much, I want to be able to help my future kids and the rest of my family in the future.'
+}
+function hobbies() {
+  document.getElementById('question').innerHTML = 'Hobbies/Interests?'
+  document.getElementById('para').innerHTML =
+    'Some of my hobbies include physical activities like running long distance races, like the LA Marathon or the Rose Bowl half marathon. Other hobbies are les active like playing video games with friends and joining summer programs such as this one.'
+}
